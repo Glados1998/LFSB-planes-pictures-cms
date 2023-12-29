3e884fc276b8427105c730eb30e7e6f91c09670c
jerome greder <j.greder@staro.ch>
2023-12-29 01:44:31 +0100

    Add actionOptions to Cloudinary plugin configuration
    Added a new 'actionOptions' section to the Cloudinary plugin configurations in the 'plugins.js' file. This includes settings for 'upload' options such as 'file', 'upload_preset', and 'media_metadata'.

--- config/plugins.js
+++ config/plugins.js
@@ -12,6 +12,13 @@ module.exports = ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {
+          file: 'https://console.cloudinary.com/console/c-c9e5feeb9523ecd23e80934736ff96/media_library/folders/c6597db30d09113371e0344fd469d8a2a2',
+          upload_preset: 'owiop47b',
+          media_metadata: true,
+        },
+      },
     },
   },
 });
